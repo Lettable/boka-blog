4d59e9d723bc38b15ef29cd9ecfc57ce2433c481
Mirza <devmirzaofficial@gmail.com>
2024-10-07 23:44:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
